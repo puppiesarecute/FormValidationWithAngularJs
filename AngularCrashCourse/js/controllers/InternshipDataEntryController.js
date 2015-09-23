3cc695d976f0b4d555ae3205fa4e7f348f4e597e
Ai.Le <trun0047@stud.kea.dk>
2015-09-23 17:38:34 +0200

    initial commit

---
+++ AngularCrashCourse/js/controllers/InternshipDataEntryController.js
@@ -0,0 +1,14 @@
+﻿var app = angular.module("internship");
+app.controller("internshipDataEntryController", function ($scope) {
+    $scope.visit = {};
+    $scope.saveVisit = function () {
+        if ($scope.visitForm.$valid) {
+            alert("Yes!");
+        }
+        else {
+            //alert("Not valid!");
+        }
+        console.log($scope.visit);
+        //$scope.visit.initials = "Frank is a nerd";
+    };
+});
